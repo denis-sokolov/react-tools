5db67fa550cfff8708a9e80fa7f53c18727d574c
Denis Sokolov <denis@sokolov.cc>
2019-12-03 12:00:00 +0200

    lib: remove test tools from the bundle

--- src/ActionArea/unexpected-action.test.tsx
+++ src/ActionArea/unexpected-action.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import test from "ava";
-import { render } from "../lib";
+import { render } from "../lib/test";
 import { ActionArea } from "./ActionArea";
 
 test("ActionArea unexpected action", t => {
