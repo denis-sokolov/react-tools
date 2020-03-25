33968ee80072f347f4d703c6490be34b10cfd601
Denis <denis@sokolov.cc>
2020-03-25 12:00:00 +0200

    new-project/webpack: comment unused example line

--- docs/new-project/webpack.config.js
+++ docs/new-project/webpack.config.js
@@ -1,7 +1,7 @@
 "use strict";
 
 const path = require("path");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+// const CopyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
 
