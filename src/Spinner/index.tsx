8b173cc3e31cfba9532784bf696bc3445dc019e5
Denis Sokolov <denis@sokolov.cc>
2020-12-04 12:00:00 +0000

    Spinner: delayed prop

--- src/Spinner/index.tsx
+++ src/Spinner/index.tsx
@@ -1,24 +1,38 @@
-import { ReactNode, useState } from "react";
+import { CSSProperties, ReactNode, useEffect, useState } from "react";
 import NanoSpinner from "react-nano-spinner";
-import { useRerenderEvery } from "../useRerender";
+import { useRerender, useRerenderEvery } from "../useRerender";
+
+const delayedTimeout = 1000;
 
 type Props = {
   children?: ReactNode;
+  delayed?: boolean;
   fullScreen?: boolean;
   timeoutMs?: number;
 };
 
 export function Spinner(props: Props) {
-  const { children, fullScreen, timeoutMs } = props;
+  const { children, delayed, fullScreen, timeoutMs } = props;
   const contents = children || <NanoSpinner />;
 
   useRerenderEvery(1000);
-
+  const rerender = useRerender();
   const [mountedAt] = useState(Date.now());
+
+  useEffect(
+    function () {
+      const timer = setTimeout(rerender, delayedTimeout + 1);
+      return () => clearTimeout(timer);
+    },
+    [rerender]
+  );
 
   const timeout = timeoutMs ?? 60000;
   if (mountedAt + timeout < Date.now())
     throw new Error("Spinner is taking too long");
+
+  const hiddenDueToDelay = delayed && Date.now() < mountedAt + delayedTimeout;
+  const delayedStyles: CSSProperties = hiddenDueToDelay ? { opacity: 0 } : {};
 
   if (fullScreen) {
     return (
@@ -33,6 +47,7 @@ export function Spinner(props: Props) {
           right: 0,
           top: 0,
           zIndex: 2,
+          ...delayedStyles,
         }}
       >
         <div>{contents}</div>
@@ -40,5 +55,5 @@ export function Spinner(props: Props) {
     );
   }
 
-  return <>{contents}</>;
+  return <div style={delayedStyles}>{contents}</div>;
 }
