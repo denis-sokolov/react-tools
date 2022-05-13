16594c1a77038d87230d03d9ceecf924c21be690
Denis Sokolov <denis@sokolov.cc>
2022-05-13 12:00:00 +0000

    esm: do not require experimental-specifier-resolution=node

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 rm -rf dist &&
 NODE_ENV=production tsc --project tsconfig.build.json &&
 NODE_ENV=production tsc --project bin/tsconfig.json &&
+find ./dist -name '*.js' -exec bash build-helper.sh {} \; &&
 cp bin/package.json dist/bin/ &&
-node -e 'console.log(JSON.stringify({ version: require("./package.json").version }))' > dist/package.json
+node -e 'console.log(JSON.stringify({ type:"module", version: require("./package.json").version }))' > dist/package.json &&
+chmod +x dist/bin/license/index.js
