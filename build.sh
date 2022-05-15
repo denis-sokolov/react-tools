8cc733cb0c7240212737c67fa59bdc4232a26ceb
Denis Sokolov <denis@sokolov.cc>
2022-05-15 12:00:00 +0000

    build: include CommonJS in the build
    Marking the package as officially esm made some
    consumers crash because in dev-server mode they
    relied on Node import behavior.

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 rm -rf dist &&
 NODE_ENV=production tsc --project tsconfig.build.json &&
+NODE_ENV=production tsc --project tsconfig.build.cjs.json &&
 NODE_ENV=production tsc --project bin/tsconfig.json &&
 find ./dist -name '*.js' -exec bash build-helper.sh {} \; &&
 cp bin/package.json dist/bin/ &&
-node -e 'console.log(JSON.stringify({ type:"module", version: require("./package.json").version }))' > dist/package.json &&
-chmod +x dist/bin/license/index.js
+node -e 'console.log(JSON.stringify({ version: require("./package.json").version }))' > dist/package.json &&
+chmod +x dist/bin/license/index.js &&
+echo '{ "type": "commonjs" }' > dist/cjs/package.json &&
+echo '{ "type": "module" }' > dist/esm/package.json
