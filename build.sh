9ca9d83a338ca5af28d472712e870d550f930989
Denis Sokolov <denis@sokolov.cc>
2022-05-16 12:00:00 +0000

    bin: clearer dependency on a field in package.json

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 rm -rf dist &&
 NODE_ENV=production tsc --project tsconfig.build.json &&
 NODE_ENV=production tsc --project bin/tsconfig.json &&
-cp bin/package.json dist/bin/
+cp bin/package.json dist/bin/ &&
+node -e 'console.log(JSON.stringify({ version: require("./package.json").version }))' > dist/package.json
