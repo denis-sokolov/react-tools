4a48b3277e8992d1f69d3756a9be0c9dabd6efdf
Denis Sokolov <denis@sokolov.cc>
2022-12-14 12:00:00 +0000

    app/dependencies: react 18

--- build-helper.sh
+++ build-helper.sh
@@ -1,9 +1,4 @@
 # Other seeming options:
 # https://www.npmjs.com/package/ts-transform-esm-import
 # https://www.npmjs.com/package/to-esm
-fix-es-imports 2>/dev/null $1 &&
-
-# Until React 18
-# https://github.com/facebook/react/issues/20235
-# -i is required on some OSs
-sed -i '.bak' 's/jsx-runtime/jsx-runtime.js/g' $1 && rm $1.bak
+fix-es-imports 2>/dev/null $1
