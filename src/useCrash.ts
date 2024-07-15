7c747446ff80ffa362a67403f203230b15df0b22
Denis Sokolov <denis@sokolov.cc>
2024-07-15 12:00:00 +0000

    useCrash: accepts { message: string }

--- src/useCrash.ts
+++ src/useCrash.ts
@@ -9,9 +9,9 @@ export function useCrash() {
   // https://github.com/facebook/react/issues/14981#issuecomment-468460187
   const [, setState] = useState(null);
   return useCallback(
-    (err: Error) =>
+    (err: Error | { message: string }) =>
       setState(() => {
-        throw err;
+        throw err instanceof Error ? err : Error(err.message);
       }),
     [setState]
   );
