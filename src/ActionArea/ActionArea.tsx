0cc1f0231036a3f8a5ce2ffd6d624540d2d82c06
Denis Sokolov <denis@sokolov.cc>
2024-11-21 12:00:00 +0000

    ActionArea: extract Action type

--- src/ActionArea/ActionArea.tsx
+++ src/ActionArea/ActionArea.tsx
@@ -8,21 +8,7 @@ import {
 
 import { scopedStyles } from "../lib";
 
-export type ActionDisabled = { disabledReason: string };
-export type ActionDownload = { download: string; url: string };
-export type ActionHandler = (() => void) | { mousedown: () => void };
-export type ActionSubmit = "submit";
-export type ActionUrl =
-  // string & {} is a hack that still accepts strings,
-  // but does not swallow the union with "submit"
-  (string & {}) | { newWindow: string };
-
-export type Action =
-  | ActionDisabled
-  | ActionDownload
-  | ActionHandler
-  | ActionSubmit
-  | ActionUrl;
+import { type Action } from "./Action";
 
 export type ActionAreaProps = {
   /**
--- src/ActionArea/index.ts
+++ src/ActionArea/index.ts
@@ -1,1 +1,2 @@
+export * from "./Action";
 export * from "./ActionArea";
